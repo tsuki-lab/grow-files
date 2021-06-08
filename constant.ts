96e5af8bfc72746054371bfa829f27951a7a9911
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-06-08 18:53:09 +0900

    コマンドからファイルを作成できるように処理を追加

---
+++ constant.ts
@@ -0,0 +1,9 @@
+export const QUESTIONS = {
+  FILE_NAME: [
+    {
+      type: 'input',
+      message: "What's file name",
+      name: 'fileName'
+    }
+  ]
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,41 @@
-console.log("HelloWorld");
+import * as fs from 'fs';
+import * as path from 'path';
+import cac from 'cac'
+import { prompt } from 'inquirer'
+import { QUESTIONS } from './constant';
+
+(async() => {
+  const cli = cac()
+
+  cli.help()
+
+  cli
+    .option('-o, --output-path [path]', 'converted file output path', {
+      default: './'
+    });
+
+  const parsed = cli.parse();
+
+  const options = {
+    fileName: parsed.args[0],
+    ...parsed.options
+  };
+
+  await inquirer(options);
+
+  console.log(options);
+  console.log(path.join(__dirname));
+
+  const data = fs.readFileSync('./templates/static-page/sample.html', 'utf-8');
+  const result = data.replace(/\$FILE_NAME/g, options.fileName);
+
+  fs.writeFileSync(`./${options.fileName}.html`, result, 'utf-8');
+})();
+
+async function inquirer(options: any) {
+  if (options.fileName === undefined) {
+    await prompt(QUESTIONS.FILE_NAME).then((answers: any) => {
+      options.fileName = (answers.fileName as string).replace(' ', '');
+    })
+  }
+};
